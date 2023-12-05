46cb7322901b25976f035b863b102a40a8bc3ffa
DevSanPoli <hemartinez16@poligran.edu.co>
2023-12-04 21:59:54 -0500

    se agregaron más opciones del menú

--- cliente.py
+++ cliente.py
@@ -1,3 +1,5 @@
+import os
+
 
 def enviar_solicitud(solicitud):
     pass
@@ -13,14 +15,50 @@ while True:
     opcion = input("Selecciona una opción: ")
     
     if opcion == '1':
+
         nombre = input("Nombre del empleado: ")
         pais = input("País: ")
         ciudad = input("Ciudad: ")
-        localizacion = input("Localización: ")
+        direccion = input("Dirección: ")
         departamento = input("Departamento: ")
         cargo = input("Cargo: ")
-        solicitud = f"insert {nombre} {pais} {ciudad} {localizacion} {departamento} {cargo}"
-        enviar_solicitud(solicitud)
+        cc =  input("Identificación:")
+
+        insertar = {
+            'nombre': nombre,
+            'pais': pais,
+            'ciudad': ciudad,
+            'direccion': direccion,
+            'departamento': departamento,
+            'cargo': cargo,
+            'cc': cc
+        }
+
+        enviar_solicitud(insertar)
+
+        os.system('clear')
+    
+    if opcion == '2':
+        identificacion = input("Escriba la identeficición del empleado a consultar: ")
+
+        enviar_solicitud(identificacion)
+        os.system('clear')
+
+    if opcion == '3':
+        identificacion = input("Escriba la identificición del empleado a eliminar: ")
+
+        enviar_solicitud(identificacion)
+        os.system('clear')
+    
+    if opcion == '4':
+        identificacion = input("Escriba la identificición del empleado a modificar: ")
+        ciudad = input("Digite la nueva ciudad del empleado: ")
+        direccion = input("Digite la nueva dirección del empleado: ß")
+
+
+        enviar_solicitud(identificacion)
+        os.system('clear')
+
     
     elif opcion == '5':
         break
